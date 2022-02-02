6a1960835525955ecdcea76f32944199c8e673a6
madhavijaival <madhavi.jaival113@gmail.com>
2022-02-01 22:32:40 -0800

    Added photo site example

--- app.js
+++ app.js
@@ -73,10 +73,13 @@ switch (day) {
 
 }
 
-// for (i = 1; i <= 20; i += 2) {
+
+/***For loops***/
+// for (let i = 1; i <= 20; i += 2) {
 //     console.log(i);
 // }
 
-for (i = 100; i >= 0; i -= 10) {
-    console.log(i);
-}
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
