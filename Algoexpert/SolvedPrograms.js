d67b25130e693c5ba70f6ce57122b7ac77fc95b8
madhavijaival <madhavi.jaival113@gmail.com>
2022-07-14 15:03:19 -0700

    new problems

--- Algoexpert/SolvedPrograms.js
+++ Algoexpert/SolvedPrograms.js
@@ -235,10 +235,10 @@ exports.nonConstructibleChange = nonConstructibleChange;
 /*
 Algorithm:  
 - FIRST INITIALIZE CLOSEST VALUE AND ADD THE CURRENT NODE VALUE OF THE TREE TO IT.
-- THEN WE HAVE TO CALCULATE THE ABSOLUTE DIFFRENCE BETWEEN THE CURRENT NODE VALUE WITH THE TARGET VALUE |TARGET -CLOSESTNODE.VLAUE|.
+- THEN WE HAVE TO CALCULATE THE ABSOLUTE DIFFRENCE BETWEEN THE CURRENT NODE VALUE WITH THE TARGET VALUE |TARGET - CLOSESTNODE.VLAUE|.
 - AND COMPARE THE DIFFRENCE IF IT SMALLER THAN THE ABSOLUTE DIFFERENCE OF THE |CLOSEST - TARGET| IF THE IT'S TRUE THEN UPDATE CLOSEST WITH THE CURRENT NODE.
 - COMPARE THIS CLOSEST WITH THE TARGET VALUE IF CURRENTNODE IS SMALL THAN THE TARGET THEN MOVE TO THE RIGHT NODES.
-- IF THE CURRENTNODE IS GREATER THA TARGET VALUE THE MOVE TO THE LEFT NODES.
+- IF THE CURRENTNODE IS GREATER THE TARGET VALUE THE MOVE TO THE LEFT NODES.
 - NOW REPEATE THE PROCESS 1- 4 UNTILL WE GET THE CLOSEST NODE AND THEN RETURN CLOSEST. 
  */
 //******************SOLUTION 1******************** */
@@ -312,7 +312,283 @@ exports.findClosestValueInBst = findClosestValueInBst;
  */
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
-//====================7. =====================================//
-
+//====================7. BRANCH SUM =====================================//
+//||TIME COMPLEXITY O(n) & SPACE COMPLEXITY O(n)
 /*ALGORITHMS:  
+- CREATE A EMPTY ARRAY TO ADD SUMS
+- CALL HELPER FUNCTION IN MAIN  & RETURN SUMS 
+- IN THE HELPER FUNCTION TAKE 3 PARAMETERS ROOT , RUNNING SUM =0, AND SUMS
+- CREATE A VARIABLE NEWRUNNIGSUM TO STORE THE VALUE OF RUNNINGSUM + NODE.VALUE
+- THEN CHECK THE CONDITION FOR CHILD NODE IF THE NODE.LEFT AND NODE.RIGHT IS EMPTY(NULL) THEN PUSH THE NEWRUNNNING SUM TO THE SUMS ARRAY
+- RETURN VLAUE
+- THEN REPEATE THIS FOR 
+    sumOfBranches(node.left, newRunningSum, sums)
+    sumOfBranches(node.right, newRunningSum, sums)
  */
+
+// This is the class of the input root.
+// Do not edit it.
+/* class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function branchSums(root) {
+  // Write your code here.
+let sums = [];
+  sumOfBranches(root, 0, sums);
+  return sums;
+  
+}
+
+function sumOfBranches(node, runningSum, sums){
+  if( node === null) return;
+const newRunningSum = runningSum + node.value;
+  if(node.left === null && node.right === null){
+    sums.push(newRunningSum);
+      return;
+  }
+
+    sumOfBranches(node.left, newRunningSum, sums)
+    sumOfBranches(node.right, newRunningSum, sums)
+  }
+
+// Do not edit the lines below.
+exports.BinaryTree = BinaryTree;
+exports.branchSums = branchSums;
+ */
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//=====================8.NODE DEPTHS : CALCULATION OF THE NODE LEVEL ==================//
+//||TIME COMPLEXITY O(n) & SPACE COMPLEXITY O(h)
+/*ALGORITHM:  
+- WE ARE CALLING THE RECURSIVE FUNCTION OF THE ORIGIONAL ONE
+- WE ADDED DEPTH TO THE MAIN FUNCTION AND ASSIGN 0 TO IT AS ROOT HAS NO LEVEL 
+- THEN WE ARE ADDING THE RIGHT NODE AND THE LEFT NODE DEPTH TO IT AND RETURNING TOTAL OF THE DEPTH.
+ */
+//Recursive algorithm way.
+/* function nodeDepths(root, depth = 0) {
+  // Write your code here.
+
+  if(!root) return 0;
+  return depth + nodeDepths(root.left, depth+1) + nodeDepths(root.right, depth+1);
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths; 
+*/
+//***************SOLUTION 2*********************/
+//ITERATIVE WAY OF THE ALGO
+/* function nodeDepths(root) {
+  // Write your code here.
+ let sumOfDepth=0;
+  const stack=[{node: root, depth:0}];
+  while(stack.length > 0){
+    const {node, depth} =stack.pop();
+    if(node === null) continue;
+    sumOfDepth += depth;
+    stack.push({node:node.left, depth: depth + 1});
+     stack.push({node:node.right, depth: depth + 1});
+  }
+  return sumOfDepth;
+}
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
+ */
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//======================= 9.DEPTH-FIRST SEARCH ====================================//
+//||TIME COMPLEXITY O(V + E) & SPACE COMPLEXITY O(V) where V = VERTEX OF TREE & E = EDGES OF THE TREE.
+/* 
+ALGORITHM: 
+- WE HAVE TO VISIT THE DEPTH VALUE OF THE LEFT SIDE CHILDRESN FIRST
+- WHEN THE LEFT CHILD IS NULL THEN IT TRAVERSE TRHOUGH THE RIGHT CHILDS
+- IT REPEAT THE PROCESS UNTILL THE ALL THE CHILD VISITED.
+- In the code first pushing the name value to the array.
+- Then looping through the childerns value.
+- Then using the child value we running the DFS function (recursively) on the child value
+- In last we are returning the array of the DFS.
+ */
+/* class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+// MAIN CODE IS HERE
+  depthFirstSearch(array) {
+    // Write your code here.
+  //First push the name value to the array
+    array.push(this.name);
+    //go through the for loop for getting childerns value
+    for(const child of this.children){
+      //then run the DFS on the child value
+      child.depthFirstSearch(array)
+    }
+    // return the array of the output
+    return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node; */
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//===================== 10. MINIMUM WAITING TIME ===================================//
+//||TIME COMPLEXITY O(nlog(n)) & SPACE COMPLEXITY O(1) 
+/*
+ALGORITHM: 
+-  
+ */
+/* function minimumWaitingTime(queries) {
+  // Write your code here.
+  queries.sort((a,b) => a - b);
+  
+ let  minWaitTime =0;
+  
+  for(let i =0; i< queries.length; i++){
+  const duration = queries[i];
+  const leftQueries =queries.length - ( i + 1);
+    minWaitTime += duration * leftQueries;
+    console.log(minWaitTime);
+  }
+  return minWaitTime;
+}
+
+// Do not edit the line below.
+exports.minimumWaitingTime = minimumWaitingTime; */
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//======================11.CLASS-PHOTO ===================================//
+//||TIME COMPLEXITY O(nlog(n)) & SPACE COMPLEXITY O(1) 
+/*
+ALGORITHM: 
+- FIRST WE NEED TO SORT ARRAY IN DECENDING ORDER.
+- THEN CREATE THE VARIABLE TO STORE THE TOPHEIGHT THEN COMPARE THE 1ST ELEMEN TFROM THE BOTH ARRAY AND RETURN THE TOP ELEMENT WITH OUTPUT OF IT'S COLOR.
+- THEN LOOP THROUGH THE INDEXES AS BOTH ARRAY HAS SAME VALUES WE NEED TO LOOP ON ONLY ONE ARRAY.
+- CHECK THE CONDITION IF THE TOPHEIGHT IS EQUAL TO THE ="COLOR ANY BLUE/RED " THE CHECK CONDITION IFR FIRST ARRAY IS >= TO SECOND THEN RETURN FALSE;
+- AND CHECK OF THE SECOND ARRAY >= FIRST ARRAY ALSO RETURN FLASE
+- IF OUR ARRANGEMNT IS RIGHT THEN SIMPLY RETURN TRUE AT END
+-  
+ */
+
+/* function classPhotos(redShirtHeights, blueShirtHeights) {
+  // Write your code here.
+  redShirtHeights.sort((a,b)=> b - a);
+   blueShirtHeights.sort((a,b)=> b - a);
+const topHeight =  redShirtHeights[0] > blueShirtHeights[0] ? 'RED' : 'BLUE';
+  
+  for(let idx =0; idx < redShirtHeights.length; idx++){
+    if(topHeight === 'BLUE'){
+      if(redShirtHeights[idx]>= blueShirtHeights[idx])
+        return false;
+      }else  if(blueShirtHeights[idx] >= redShirtHeights[idx])
+        return false;
+       }
+    
+  return true;
+} 
+*/
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//========================== 12.TANDEM BICYCLE ===============================//
+//||TIME COMPLEXITY O(nlog(n)) & SPACE COMPLEXITY O(1) 
+/* 
+ALGORITHM:
+- FIRST WE NEED TO SORT BLUESHIRT ARRAY IN THE REVERSE ORDER.
+- THEN WE CHECK IF THE FASTEST IS FALSE THEN WE SORT REDSHIRT ARRAY IN DECENDING ORDER TO GET THE LOWEST TOTAL
+- ELSE WE SORT THE REDSHIRT ARRAY IN THE ASSENDING ORDER.
+- THNE CRATE VERIABLE TO STORE THE TOTALSPEED AND RUN THE FOR LOOP FOR ITERATING THE VALUES
+-THEN WE ADD THE VALUE WHICH ONE IS THE MAX FRIOM THE REDSHIRT OR BLUESHIRT.
+- IN LAST RESTUR TOTTALSPEED
+ */
+/* function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+  // Write your code here.
+  blueShirtSpeeds.sort((a,b)=> b - a);
+  
+  if(!fastest){
+    redShirtSpeeds.sort((a,b)=> b - a); 
+  }else {
+    redShirtSpeeds.sort((a,b)=> a - b);
+  }
+let totalSpeed=0;
+  for(let i=0; i<blueShirtSpeeds.length; i++){
+    totalSpeed +=Math.max(redShirtSpeeds[i],  blueShirtSpeeds[i]);
+  }
+  return totalSpeed;
+}
+
+// Do not edit the line below.
+exports.tandemBicycle = tandemBicycle;
+
+ */
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//=======================13.REMOVE DUPLICATES FROM THE LINKED-LIST ================================//
+//||TIME COMPLEXITY O(n) & SPACE COMPLEXITY O(1) 
+/*
+ALGORITHM:
+- FIRST WE NEED TO CREATE THE CURRRENTNODE VARIABLE TO STORE OUR HEAD NODE.
+- THEN  CHECK CONDITION IF THE CURRENTNODE IS NOT NULL IS TRUE THEN WE ASSIGN THE NEXTDISTNODE = CURRENTNODE.NEXT VALUE;
+- THEN WHILE LOOP FOR THE NEXTDIST IS NOT NULL AND IF IT'S VALUE IS EQUAL TO THE CURRENTNODE VALUE.
+- IT IT'S TRUE THEN WE MOVE OUR NEXTDIST NODE TO THE NEXT OF THE NEXTDISTNODE = NEXTDISTNODE.NEXT
+- AND THEN IN FINALLY WE MOVE OUR POINT TO THE OUR "CURRENTNODE.NEXT" TO THE NEXTDISTNODE AND "CURRENTNODE" = NEXTDISTNODE.
+
+- WE ARE NOT GETTING ANY O(N^2) TIME BECAUSE WE AREN'T REPEATATING THE VALUE ACTUALLY WE ARE ELIMENATING THOSE.
+
+ */
+/* // This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  // Write your code here.
+  let currentNode =linkedList;
+  while(currentNode!== null){
+    let nextDistNode = currentNode.next; 
+    while(nextDistNode !== null && nextDistNode.value === currentNode.value ){
+      nextDistNode = nextDistNode.next;
+    }
+
+    currentNode.next = nextDistNode;
+    currentNode = nextDistNode;
+  }
+  return linkedList;
+  
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
+ */
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//=====================14. ==========================================
