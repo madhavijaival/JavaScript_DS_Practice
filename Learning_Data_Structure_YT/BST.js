479dd0b826f4397449ddbab3724e032dae7341bf
madhavijaival <madhavi.jaival113@gmail.com>
2023-02-08 22:14:48 -0800

    BST code

--- Learning_Data_Structure_YT/BST.js
+++ Learning_Data_Structure_YT/BST.js
@@ -1,3 +1,5 @@
+//construction of a Binary search tree with Insertion, Search//
+
 class Node{
     constructor(value){
       this.value = value;
@@ -64,19 +66,85 @@ class Node{
         }
     
       }
-      
+
+      //=================DFS Traversal==============//
+      preOrder(root){
+        if(root){
+          console.log(root.value);
+          this.preOrder(root.left);
+          this.preOrder(root.right);
+        }
+      }
+      inOrder(root){
+        if(root){
+          this.inOrder(root.left);
+          console.log(root.value);
+          this.inOrder(root.right);
+          
+        }
+      }
+      postOrder(root){
+        if(root){
+          this.postOrder(root.left);
+          this.postOrder(root.right);
+          console.log(root.value);
+        }
+      }
+        
+   //========= BFS Traversal ===============//
+
+levelOrder(){
+
+  const queue = [];
+  queue.push(this.root);
+  while(queue.length){
+    let curr = queue.shift();
+    console.log(curr.value);
+    if(curr.left){
+      queue.push(curr.left);
+    }
+    if(curr.right){
+      queue.push(curr.right);
+       }
+     }
+   }   
+//=== getting min- max values from the tree ===//
+min(root){
+  if(!root.left){
+    return root.value;
+  }else{
+    return this.min(root.left);
   }
-    
-  
-  
-  const bst = new BinarySearchTree();
-  console.log("Tree is empty?",  bst.isEmpty());
-  bst.insert(5);
-  bst.insert(10);
-  bst.insert(11);
-  
-  console.log(bst.search(bst.root, 10));
-  console.log(bst.search(bst.root, 5));
-  console.log(bst.search(bst.root, 11));
-  
-  
+}
+
+max(root){
+  if(!root.right){
+    return root.value;
+  }else{
+    return this.max(root.right);
+    }
+  }
+}
+
+
+
+const bst = new BinarySearchTree();
+console.log("Tree is empty?",  bst.isEmpty());
+bst.insert(5);
+bst.insert(10);
+bst.insert(7);
+bst.insert(15)
+bst.insert(2);
+
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 11));
+
+console.log(bst.min(bst.root));
+console.log(bst.max(bst.root));
+
+//bst.levelOrder();
+
+//bst.inOrder(bst.root);
+//bst.preOrder(bst.root);
+//bst.postOrder(bst.root);
